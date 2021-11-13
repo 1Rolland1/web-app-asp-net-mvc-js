9b18fa3097f7615df3dbb5248866377d654fcc50
root <vladislav.2000@inbox.ru>
2021-11-13 21:45:02 +1000

    init

---
+++ web-app-asp-net-mvc-js/Controllers/DisciplinesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using web_app_asp_net_mvc_js.Models;
+
+namespace web_app_asp_net_mvc_js.Controllers
+{
+    public class DisciplinesController : Controller
+    {
+        private readonly string _key = "123456Qq";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var db = new TimetableContext();
+            var disciplines = db.Disciplines.ToList();
+
+            return View(disciplines);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var discipline = new Discipline();
+            return View(discipline);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Discipline model)
+        {
+            var db = new TimetableContext();
+            
+            if (!ModelState.IsValid)
+            {
+                var disciplines = db.Disciplines.ToList();
+                ViewBag.Create = model;
+                return View("Index", disciplines);
+            }
+
+            
+            if (model.Key != _key)
+                ModelState.AddModelError("Key", "Ключ для создания/изменения записи указан не верно");
+            if (!ModelState.IsValid)
+            {
+                var disciplines = db.Disciplines.ToList();
+                ViewBag.Create = model;
+                return View("Index", disciplines);
+            }
+            db.Disciplines.Add(model);
+            db.SaveChanges();
+
+            return RedirectPermanent("/Disciplines/Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var db = new TimetableContext();
+            var discipline = db.Disciplines.FirstOrDefault(x => x.Id == id);
+            if (discipline == null)
+                return RedirectPermanent("/Disciplines/Index");
+
+            db.Disciplines.Remove(discipline);
+            db.SaveChanges();
+
+            return RedirectPermanent("/Disciplines/Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new TimetableContext();
+            var discipline = db.Disciplines.FirstOrDefault(x => x.Id == id);
+            if (discipline == null)
+                return RedirectPermanent("/Disciplines/Index");
+
+            return View(discipline);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Discipline model)
+        {
+            var db = new TimetableContext();
+            var discipline = db.Disciplines.FirstOrDefault(x => x.Id == model.Id);
+            if (discipline == null)
+                ModelState.AddModelError("Id", "Дисциплина не найдена");
+            if (model.Key != _key)
+                ModelState.AddModelError("Key", "Ключ для создания/изменения записи указан не верно");
+            if (!ModelState.IsValid)
+            {
+                var disciplines = db.Disciplines.ToList();
+                ViewBag.Create = model;
+                return View("Index", disciplines);
+            }
+
+            MappingDiscipline(model, discipline, db);
+
+            db.Entry(discipline).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectPermanent("/Disciplines/Index");
+        }
+
+        private void MappingDiscipline(Discipline sourse, Discipline destination, TimetableContext db)
+        {
+            destination.Name = sourse.Name;
+            destination.DisciplineGoal = sourse.DisciplineGoal;
+            destination.DisciplineObjectives = sourse.DisciplineObjectives;
+            destination.MainSections = sourse.MainSections;
+            destination.Key = sourse.Key;
+
+
+        }
+
+    }
+}
---
+++ web-app-asp-net-mvc-js/Scripts/modal.js
@@ -0,0 +1,30 @@
+﻿function GetCreate(controllerName, actionName) {
+    var x = new XMLHttpRequest();
+    x.open("GET", `/${controllerName}/${actionName}`);
+    x.onload = function () {
+        var bodyModal = document.getElementById(`modal-body-${controllerName}`);
+        bodyModal.innerHTML = x.responseText;
+    }
+    x.send(null);
+}
+
+function Post(controllerName, actionName, enctype) {
+    var x = new XMLHttpRequest();
+
+    var form = document.querySelector('form');
+    var data = new FormData(form);
+    x.open("POST", `/${controllerName}/${actionName}`);
+    x.setRequestHeader('Content-Type', `${enctype}`);
+    x.send(data);
+}
+
+
+function GetEdit(controllerName, actionName, id) {
+    var x = new XMLHttpRequest();
+    x.open("GET", `/${controllerName}/${actionName}?id=${id}`);
+    x.onload = function () {
+        var bodyModal = document.getElementById(`modal-body-${controllerName}`);
+        bodyModal.innerHTML = x.responseText;
+    }
+    x.send(null);
+}
